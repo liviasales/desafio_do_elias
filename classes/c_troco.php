59ca65f179865b3ffe0b18dc35fc20f23341d71f
liviasales <63153694+liviasales@users.noreply.github.com>
2021-12-27 09:47:44 -0300

    Update c_troco.php
    Modificação da classe para troco

--- classes/c_troco.php
+++ classes/c_troco.php
@@ -1,59 +1,14 @@
 <?php
     class c_troco {
-        public function getQtdeNotas($reais) {
-            $qtdeNotas = [
-              "100" => 0,
-               "50" => 0,
-               "20" => 0,
-               "10" => 0,
-                "5" => 0,
-                "2" => 0,
-            ];  
-   
-            if ($reais == 100) {
-                $qtdeNotas["100"] = 1;
+        public function troco($valor) {
+            $r = [0,0,0,0,0,0];
+            $notas = [100,50,20,10,5,2];
+
+            for($i = 0; $i < count($r); $i++) {
+                $r[$i] = floor($valor / $notas[$i]);
+                $valor -= $r[$i] * $notas[$i];
             }
-            if ($reais == 100) {
-                $qtdeNotas["50"] = 1;
-            }
-            if ($reais == 20) {
-                $qtdeNotas["20"] = 1;
-            }
-            if ($reais == 10) {
-                $qtdeNotas["10"] = 1;
-            }
-            if ($reais == 5) {
-                $qtdeNotas["5"] = 1;
-            }
-            if ($reais == 2) {
-                $qtdeNotas["2"] = 1;
-            }   
-            return $qtdeNotas;
-        }
-        public function getQtdeMoedad($centavos) {
-            $qtdeMoedas = [
-                "1" => 0,
-                "0.5" => 0,
-                "0.25" => 0,
-                "0.1" => 0,
-                "0.05" => 0,
-            ];
-            if ($reais == 1) {
-                $qtdeMoedas["1"] = 1;
-            }
-            if ($reais == 0.5) {
-                $qtdeMoedas["0.5"] = 1;
-            }
-            if ($reais == 0.25) {
-                $qtdeMoedas["0.25"] = 1;
-            }
-            if ($reais == 0.1) {
-                $qtdeMoedas["0.1"] = 1;
-            }
-            if ($reais == 0.05) {
-                $qtdeMoedas["0.05"] = 1;
-            }
-            return $qtdeMoedas;
+            return $r;
         }
     }
 ?>
