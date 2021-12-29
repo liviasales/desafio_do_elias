49abe5ee9725f9ed8b6188efd90f3c189255a154
liviasales <63153694+liviasales@users.noreply.github.com>
2021-12-29 09:13:34 -0300

    Update cont_cobranca.php
    controler atualizado

--- cont_cobranca.php
+++ cont_cobranca.php
@@ -5,7 +5,11 @@
     $troco = new c_troco;
     $valorTotal = $_POST['valorTotal'];
     $valorPago = $_POST['valorPago'];
+    $valorTroco = $valorTotal - $valorPago;
     if ($valorPago > $valorTotal) {
-        return $troco;
+        $_SESSION['cobranca']['mensagem'] .= 'Seu troco é de R$'.$valorTroco.',00<br>';
+    } else {
+        $_SESSION['cobranca']['mensagem'] .= 'Você não possui troco!!';
+        $_SESSION['cobranca']['alerta'] = 'success';
     }
 ?>
