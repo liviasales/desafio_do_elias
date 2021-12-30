715dfc4ae0d4fade9f72197d358d1ef2dcb753c0
liviasales <63153694+liviasales@users.noreply.github.com>
2021-12-30 12:39:51 -0300

    class and controller changes
    Mudanças feitas na estrutura da classe e do controller

--- classes/c_troco.php
+++ classes/c_troco.php
@@ -1,14 +1,14 @@
 <?php
     class c_troco {
-        function troco($valor) {
-            $troco = [0,0,0,0,0,0];
-            $notas = [100,50,20,10,5,2];
+        public $troco = [0,0,0,0,0,0];
+        public $notas = [100,50,20,10,5,2];
+        public function troco($valor) {           
 
-            for($i = 0; $i < count($troco); $i++) {
-                $troco[$i] = floor($valor / $notas[$i]);
-                $valor -= $troco[$i] * $notas[$i];
+            for($i = 0; $i < count($this -> troco); $i++) {
+                $this -> troco[$i] = floor($this -> valor / $this -> notas[$i]);
+                $this -> valor -= $this -> troco[$i] * $this -> notas[$i];
             }
-            return $troco;
+            return $this -> troco;
         }
     }
 ?>
--- cont_cobranca.php
+++ cont_cobranca.php
@@ -2,7 +2,7 @@
     session_start();
     $_SESSION['cobranca']['mensagem'] = '';
     $_SESSION['cobranca']['alerta'] = 'danger';
-    $troco = new c_troco;
+    $troco = new c_troco();
     $valorTotal = $_POST['valorTotal'];
     $valorPago = $_POST['valorPago'];
     $valorTroco = $valorTotal - $valorPago;
